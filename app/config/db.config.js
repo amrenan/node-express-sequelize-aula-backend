bbd27d73ddf904aacf3bf35903d9933645427ae6
amrenan <mrenan@live.com>
2023-09-02 11:11:10 -0300

    add config e connectando no banco

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     HOST:"localhost",
-    USER: "",
-    PASSWORD: "",
+    USER: "postgres",
+    PASSWORD: "123",
     DB: "itemsdb",
-    dialect: "mysql",
+    dialect: "postgres",
     pool: {
         max: 5,
         min: 0,
-        acquire: 3000,
+        acquire: 30000,
         idle: 10000
     }
 };
